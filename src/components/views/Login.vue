9519f8dc0c93c1c6a54285c97ac78ec6334d136b
KatsutoshiFujishima <k-fujishima@ac-tec.co.jp>
2025-01-09 20:52:27 +0900

    画面作成

--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -1,39 +1,54 @@
 <template>
-    <p class="text-h5 my-2">ログイン</p>
-
+    <v-container>
+    <v-form
+      v-model="isValid"
+    >
+    <v-row></v-row>       
     <v-row>
         <v-col cols="2">
-            <p class="text-h6">ログインID</p>
+            <p class="text-h5">ログインID</p>
         </v-col>
     
         <v-col>
             <v-responsive
                 max-width="344"
-                max-height="50"
+                max-height="80"
             >
                 <v-text-field
-                type="input"
+                    type="input"
+                    variant="outlined"
+                    bg-color="blue-grey-lighten-4"
+                    maxlength="20"
+                    v-model="loginid"
+                    :rules="[
+                        (v) => v.length > 0 || 'ログインIDを入力してください',
+                    ]"
                 ></v-text-field>
             </v-responsive>
-            <p class="text-h6">ログインIDを入力してください</p>
         </v-col>
     </v-row>
 
     <v-row>
         <v-col cols="2">
-            <p class="text-h6">パスワード</p>
+            <p class="text-h5">パスワード</p>
         </v-col>
 
         <v-col>
             <v-responsive
                 max-width="344"
-                max-height="50"
+                max-height="80"
             >
                 <v-text-field
-                type = "password"
+                    type = "password"
+                    variant="outlined"
+                    bg-color="blue-grey-lighten-4"
+                    maxlength="20"
+                    v-model="password"
+                    :rules="[
+                        (v) => v.length > 0 || 'パスワードを入力してください',
+                    ]" 
                 ></v-text-field>
             </v-responsive>
-            <p class="text-h6">パスワードを入力してください</p>
         </v-col>
     </v-row>
     <v-row>
@@ -41,4 +56,12 @@
             ログイン
         </v-btn>
     </v-row>
+    </v-form>
+    </v-container>
 </template>
+<script setup lang="ts">
+    import { ref } from 'vue';
+    const loginid = ref('');
+    const password = ref('');
+    const isValid = ref(false);
+</script>
