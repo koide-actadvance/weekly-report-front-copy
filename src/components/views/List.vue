371a74dfaef693d66a299fbce12b6c4e17d3ea13
Yarimizu1 <t-yarimizu@ac-tec.co.jp>
2025-02-25 17:34:48 +0900

    test

--- src/components/views/List.vue
+++ src/components/views/List.vue
@@ -2,6 +2,9 @@
   <v-container>
     <p class="text-h5 my-2">週報一覧画面（週次報告書）</p>
 
+    <!--  <v-date-picker v-model="range" range></v-date-picker> -->
+    <!--  <p>選択された範囲: {{ range }}</p> -->
+
     <div>
       <p class="text-h3">週報一覧</p>
     <v-data-table
@@ -47,6 +50,7 @@ export default {
         {no: 1,pref_jp: "山田",pref_en: "Advance",area: "A社",population: "NDI", eigyo:"宮川L",kikan:"XXXX"},
         {no: 2,pref_jp: "鈴木",pref_en: "Act",area: "B社",population: "NEC", eigyo:"秋山L",kikan:"XXXX"}
       ],
+      range: null, // 配列で範囲を管理 ['2024-12-01', '2024-12-10']
     }
   }
 }
